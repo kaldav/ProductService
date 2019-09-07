42b3dc58e99a0a011cc37f58076278c49a241358
daka01 <david.kalacska@betssongroup.com>
2019-09-07 21:44:45 +0200

    removed some methods

--- ProductService/Controllers/ProductSearchController.cs
+++ ProductService/Controllers/ProductSearchController.cs
@@ -12,35 +12,14 @@ namespace ProductService.Controllers
     [ApiController]
     public class ProductSearchController : ControllerBase
     {
-        // GET: api/ProductSearch
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/ProductSearch/5
         [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "value";
         }
-
-        // POST: api/ProductSearch
+        
         [HttpPost]
         public void Post([FromBody] SearchQuery searchQuery)
-        {
-        }
-
-        // PUT: api/ProductSearch/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
         {
         }
     }
